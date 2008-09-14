7cdb39b11c273cdc60ca05986f884185d2b6af3c
Jan-Frode Myklebust <janfrode@tanso.net>
2008-09-14 22:56:49 +0200

    Remove hardcoded toplevel directory so that the script can be used on other machines than the main webserver.

--- makeall.sh
+++ makeall.sh
@@ -1,4 +1,11 @@
 #! /bin/sh -
 
-find . -name "index.txt" -exec touch '{}' \;
-for i in $(find . -name "index.txt" -print) ; do cd $(dirname $i) ;  make ; cd /var/www/html/ ; done
+# Make sure we're at the top level of the website, where "makeall.sh" lives:
+TOPLEVEL=$PWD
+cd $TOPLEVEL
+
+if test -f makeall.sh ; then
+    
+    find . -name "index.txt" -exec touch '{}' \;
+    for i in $(find . -name "index.txt" -print) ; do cd $(dirname $i) ;  make ; cd $TOPLEVEL ; done
+fi
